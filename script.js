ea9f421295866e03846131ac65e4aabba47a4815
George Kerr <george.kerr24@students.bhasvic.ac.uk>
2025-10-13 15:44:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const game_data = {
     population: 0,
     money: 0,
-    money_per_second: 0
-
+    money_per_second: 0,
+    workers: 0,
+    worker_price: 100,
+    worker_gen: 1
 }
 
 // Click event listener for planet button
@@ -23,5 +25,20 @@ setInterval(() => {
     document.getElementById("money").textContent = game_data.money
 }, 1000)
 
+
+
+document.getElementById("worker-button").addEventListener("click", function (evenet) {
+    if (game_data.money > game_data.worker_price) {
+        console.log("test")
+        game_data.workers ++
+        game_data.worker_gen = game_data.workers
+        game_data.money -= game_data.worker_price
+        game_data.worker_price *= 1.5
+        document.getElementById("workers").textContent = game_data.workers
+        document.getElementById("worker-price").textContent = game_data.worker_price
+        document.getElementById("worker-gen").textContent = game_data.worker_gen
+    }
+ 
+})
 
 
