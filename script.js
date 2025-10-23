37992469c7c382c721bb06e6ddf020d2f240fc46
George Kerr <george.kerr24@students.bhasvic.ac.uk>
2025-10-23 11:40:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
+// ========== GAME DATA ========== //
+
 const game_data = {
     population: 0,
     money: 0,
     money_per_second: 0,
     worker_price: 100,
-    worker_price_multiplier: 0
+    worker_price_multiplier: 0.1
 }
 
+// ========== FUNCTIONS ========== //
+
+function increaseMoney() {
+    game_data.money += game_data.money_per_second
+    game_data.money = Math.round(game_data.money * 100) / 100 //Removes extra, unwanted decimal places 
+    document.getElementById("population").textContent = game_data.population
+}
+
+function workerClick() {
+    game_data.population += 1
+    game_data.population = Math.round(game_data.population * 100) / 100 //Removes extra, unwanted decimal places 
+    document.getElementById("money").textContent = game_data.money
+}
+
+
+// ========== EVENT LISTENERS ========== //
+
 // Click event listener for planet button
-// Event handler updates population and money_per_seoncd, and updates the population and money per second display in the HTML
+// Event handler code updates population and money_per_seoncd, and updates the population and money per second display in the HTML
 document.getElementById("planet-button").addEventListener("click", function (event) { 
     game_data.population ++;                                                                      
     game_data.money_per_second = game_data.population * 0.01   
@@ -17,19 +36,10 @@ document.getElementById("planet-button").addEventListener("click", function (eve
 })
 
 
-// Loop that runs every second, increases money by money_per_second and updates money display in the HTML
-setInterval(() => {
-    game_data.money += game_data.money_per_second
-    game_data.money = Math.round(game_data.money * 100) / 100 //Removes extra, unwanted decimal places 
-    document.getElementById("money").textContent = game_data.money
-}, 1000)
-
-
-document.getElementById("worker-button").addEventListener("click", function (evenet) {
+document.getElementById("worker-button").addEventListener("click", function (event) {
     if (game_data.money > game_data.worker_price) {
-        game_data.worker_price_multiplier = 0.1
         game_data.workers ++
-        game_data.worker_gen = game_data.workers
+        game_data.worker_gen = game_data.workers 
         game_data.money -= game_data.worker_price
         game_data.worker_price *= 1 + game_data.worker_price_multiplier
         game_data.worker_price_multiplier += 0.05
@@ -41,8 +51,14 @@ document.getElementById("worker-button").addEventListener("click", function (eve
  
 })
 
+// ========== LOOPS ========== //
+
+// Loop that runs every second, which calls the increaseMoney() Function
 setInterval(() => {
-    game_data.population += 1
-    game_data.population = Math.round(game_data.population * 100) / 100 //Removes extra, unwanted decimal places 
-    document.getElementById("money").textContent = game_data.money
+    increaseMoney()
+}, 1000)
+
+// Loop that runs every 2.5 seconds, which calls the workerClick() Function
+setInterval(() => {
+    workerClick()
 }, 2500)
