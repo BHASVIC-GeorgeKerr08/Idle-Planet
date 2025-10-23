65188ac344835498788e1b643de581b4a5802f0b
George Kerr <george.kerr24@students.bhasvic.ac.uk>
2025-10-23 22:50:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
 // ========== GAME DATA ========== //
 
 const game_data = {
+    // POPULATION AND MONEY
     population: 0,
     money: 0,
-    money_per_second: 0,
+    money_per_second: 0, 
+
+    // WORKERS
+    workers: 0,
+    worker_speed: 2.5, // The amount of time (in seconds) that each worker takes to increase the population
+    worker_increase:  1, // The amount of population that each worker increases every action
+    total_worker_gen: 0, // The amount of population that all workers combined are generating per second
     worker_price: 100,
-    worker_price_multiplier: 0.1
+    worker_price_multiplier: 0.1 
 }
 
 // ========== FUNCTIONS ========== //
 
-function increaseMoney() {
-    game_data.money += game_data.money_per_second
-    game_data.money = Math.round(game_data.money * 100) / 100 //Removes extra, unwanted decimal places 
-    document.getElementById("population").textContent = game_data.population
-}
-
-function workerClick() {
-    game_data.population += 1
-    game_data.population = Math.round(game_data.population * 100) / 100 //Removes extra, unwanted decimal places 
-    document.getElementById("money").textContent = game_data.money
-}
 
 
 // ========== EVENT LISTENERS ========== //
@@ -39,14 +35,18 @@ document.getElementById("planet-button").addEventListener("click", function (eve
 document.getElementById("worker-button").addEventListener("click", function (event) {
     if (game_data.money > game_data.worker_price) {
         game_data.workers ++
-        game_data.worker_gen = game_data.workers 
         game_data.money -= game_data.worker_price
+
         game_data.worker_price *= 1 + game_data.worker_price_multiplier
+        game_data.worker_price = Math.round(game_data.worker_price * 100) / 100
         game_data.worker_price_multiplier += 0.05
-        game_data.worker_price_multiplier = Math.round(game_data.worker_price_multiplier * 100) / 100
+
+        game_data.total_worker_gen = (game_data.workers * game_data.worker_increase) 
+
         document.getElementById("workers").textContent = game_data.workers
         document.getElementById("worker-price").textContent = game_data.worker_price
-        document.getElementById("worker-gen").textContent = game_data.worker_gen
+        document.getElementById("worker-gen").textContent = game_data.total_worker_gen / game_data.worker_speed
+
     }
  
 })
@@ -55,10 +55,20 @@ document.getElementById("worker-button").addEventListener("click", function (eve
 
 // Loop that runs every second, which calls the increaseMoney() Function
 setInterval(() => {
-    increaseMoney()
+    game_data.money += game_data.money_per_second
+    game_data.money = Math.round(game_data.money * 100) / 100 //Removes extra, unwanted decimal places 
+    document.getElementById("money").textContent = game_data.money
 }, 1000)
 
-// Loop that runs every 2.5 seconds, which calls the workerClick() Function
+
 setInterval(() => {
-    workerClick()
-}, 2500)
+    game_data.population += game_data.total_worker_gen
+    game_data.population = Math.round(game_data.population * 100) / 100 //Removes extra, unwanted decimal places 
+
+    game_data.money_per_second = game_data.population * 0.01   
+    game_data.money_per_second = Math.round(game_data.money_per_second * 100) /100 //Removes extra, unwanted decimal places 
+
+    document.getElementById("population").textContent = game_data.population
+    document.getElementById("money-per-second").textContent = game_data.money_per_second
+    
+}, game_data.worker_speed * 1000)
